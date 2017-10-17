68aa4b6d1c02a5707a309c285c335762235689f2
felixdietrich <felix.dietrich@unisg.ch>
2017-10-17 15:59:46 +0200

    plus monthly

--- p2/2909.R
+++ p2/2909.R
@@ -1,82 +1,70 @@
 require(stargazer)
 require(quantmod)
 ### 2017
-FD1b <- readRDS('~/Dropbox/newdiss/G10-indices.rds')
-FD1c <- readRDS('~/Dropbox/newdiss/G10_Fut_Cont_Index.rds')
+FD1b <- readRDS('~/Dropbox/newdiss/G10-indices.rds') # from 2004 # aus FuturesCarryd_newInd.xlsx
+FD1c <- readRDS('~/Dropbox/newdiss/G10_Fut_Cont_Index.rds') # long-term
 all.equal(FD1b, FD1c['2004-03-31/2014'], check.attributes=FALSE) # same only the ordering is different
 
-JPM <- readRDS('~/Dropbox/newdiss/files/Oct_JPMVXYG7.rds')['2004-03-31/2014-12-31']
-stockm <- cbind(getSymbols('^GSPC', from='2004-03-31', to='2014-12-31', auto.assign = F)[,4],
-             getSymbols('VTI', from='2004-03-31', to='2014-12-31', auto.assign = F)[,6],
-             getSymbols('^VIX', from='2004-03-31', to='2014-12-31', auto.assign = F)[,4],
-             JPM)
-stockm2 <- diff(log(stockm))[2:nrow(stockm),] # remove first NA
-
-stockm3 <- cbind(summa[,1],stockm2)[,2:4] # nrow(stockm3[which(is.na(stockm3[,2])),]) # 98 === DUMMY DAMIT SELBER INDEX
-nrow(stockm2)
-
-stockm4=na.omit(diff(log(apply.monthly(stockm, last))))
-
-FD1b <- readRDS('~/Dropbox/newdiss/G10-indices.rds') # aus FuturesCarryd_newInd.xlsx
-
 ### SUMMA DAILY
-head(FD1b)
-summa=diff(log(FD1b))
-summa=cbind(summa[2:nrow(summa),],
-            readRDS('~/Dropbox/newdiss/files/longLEG.rds'),
-            readRDS('~/Dropbox/newdiss/files/shortLEG.rds'),
-            'HML'=readRDS('~/Dropbox/newdiss/files/total-Reb.rds'))
+summa <- diff(log(FD1b))
+summa <- cbind(summa[2:nrow(summa),],
+               readRDS('~/Dropbox/newdiss/files/longLEG.rds'),
+               readRDS('~/Dropbox/newdiss/files/shortLEG.rds'),
+               'HML'=readRDS('~/Dropbox/newdiss/files/total-Reb.rds'))
 # which.na.xts(summa[,'NOK'])
-head(summa)
+head(summa) # from 2004-04-01 / 2014
 plot(cumsum(summa[,'longLeg']))
+plot(summa$CHF)
+plot(FD1b$CHF)
+plot(FD1b$JPY)
 
-### DAILY
+stockm <- cbind(getSymbols('^GSPC', from='1990-03-31', to='2014-12-31', auto.assign = F)[,4],
+                getSymbols('VTI', from='1990-03-31', to='2014-12-31', auto.assign = F)[,6],
+                getSymbols('^VIX', from='1990-03-31', to='2014-12-31', auto.assign = F)[,4],
+                readRDS('~/Dropbox/newdiss/files/Oct_JPMVXYG7.rds'))
+stockm <- diff(log(stockm)) # [2:nrow(stockm),] # remove first NA
+
+# stockm3 <- cbind(summa[,1],stockm2)[,2:4] # nrow(stockm3[which(is.na(stockm3[,2])),]) # 98 === DUMMY DAMIT SELBER INDEX
+# nrow(stockm2)
+
+daily <- cbind(summa, stockm)['2004-04-01/2014-12-31']
+monthly <- apply.monthly(daily, colSums, na.rm = T)
+# stockm4=na.omit(diff(log(apply.monthly(stockm, last))))
+tail(daily, 20)
+
+### DAILY ----
 multi.fun <- function(x) {
   c(MEAN = mean(x, na.rm=T), SD = sd(x, na.rm=T), 
     SKEW = skewness(x, na.rm=T), KURT = kurtosis(x, na.rm=T),
     MAX = max(x, na.rm=T), MIN = min(x, na.rm=T))
 }
 
-asd = sapply(summa, multi.fun)
-round(asd[c(1,2),], 3)
+asd = sapply(daily[,c(1:ncol(summa))]*100, multi.fun)
+asd = rbind(asd, 
+            'SP'=apply(daily[,c(1:ncol(summa))], 2, function(x) cor(x, daily[,'GSPC.Close'], use='complete.obs')),
+            'TOT'=apply(daily[,c(1:ncol(summa))], 2, function(x) cor(x, daily[,'VTI.Adjusted'], use='complete.obs')),
+            'VIX'=apply(daily[,c(1:ncol(summa))], 2, function(x) cor(x, daily[,'VIX.Close'], use='complete.obs')))
 
-dd
-dd=rbind(
-  MEAN = round(apply(summa*100,2,mean,na.rm = T),3),
-  SD = round(apply(summa*100,2,sd,na.rm = T),3),
-  SKEW = round(apply(summa*100,2,skewness,na.rm = T),2),
-  KURT = round(apply(summa*100,2,kurtosis,na.rm = T),2),
-  MAX = round(apply(summa*100,2,max,na.rm = T),2),
-  MIN = round(apply(summa*100,2,min,na.rm = T),2),
-  SP=round(apply(summa,2,function(x) cor(stockm3[,1],x, use='complete.obs')),2),
-  TOT=round(apply(summa,2,function(x) cor(stockm3[,2],x, use='complete.obs')),2),
-  VIX=round(apply(summa,2,function(x) cor(stockm3[,3],x, use='complete.obs')),2)
-)
-stargazer(dd, digits=NA)
+round(asd[c(1:2),], 3)
+stargazer(asd)
 
-### MONTHLY
-summa=apply.monthly(summa, colSums)
-mm=rbind(
-  MEAN = round(apply(summa,2,mean,na.rm = T) * 100 * 12,3),
-  SD = round(apply(summa,2,sd,na.rm = T) * 100 * sqrt(12),3),
-  SR = round((apply(summa,2,mean,na.rm = T) * 12)/(apply(summa,2,sd,na.rm = T) * sqrt(12)),3),
-  SKEW = round(apply(summa,2,skewness,na.rm = T),2),
-  KURT = round(apply(summa,2,kurtosis,na.rm = T),2),
-  MAX = round(apply(summa,2,max,na.rm = T) * 100,2),
-  MIN = round(apply(summa,2,min,na.rm = T) * 100,2),
-  SP=round(apply(summa,2,function(x) cor(stockm4[,1],x, use='complete.obs')),2),
-  TOT=round(apply(summa,2,function(x) cor(stockm4[,2],x, use='complete.obs')),2),
-  VIX=round(apply(summa,2,function(x) cor(stockm4[,3],x, use='complete.obs')),2)
-)
-mm
-stargazer(mm, digits=NA)
-summa
+DBCFHX <- cbind(diff(log(readRDS('~/Dropbox/newdiss/files/Oct_DBCFHX.rds'))), stockm)['1993-03-15/2014']
+multi.fun(DBCFHX[,1]*100)
+cor(DBCFHX[,'DBCFHX'], DBCFHX[,'GSPC.Close'], use='complete.obs')
+cor(DBCFHX[,'DBCFHX'], DBCFHX[,'VIX.Close'], use='complete.obs')
 
-plot(summa$CHF)
-plot(FD1b$CHF)
-plot(FD1b$JPY)
+# MONTHLY ----
 
+asd = sapply(monthly[,c(1:ncol(summa))]*100, multi.fun)
+asd = rbind(asd, 
+            'SP'=apply(monthly[,c(1:ncol(summa))], 2, function(x) cor(x, monthly[,'GSPC.Close'], use='complete.obs')),
+            'TOT'=apply(monthly[,c(1:ncol(summa))], 2, function(x) cor(x, monthly[,'VTI.Adjusted'], use='complete.obs')),
+            'VIX'=apply(monthly[,c(1:ncol(summa))], 2, function(x) cor(x, monthly[,'VIX.Close'], use='complete.obs')))
 
+round(asd[c(1:2),], 3)
+stargazer(asd)
+
+### OTHER TRY ----
 require(tseries)
 # jarque.bera.test(summa[,6])
 # SharpeRatio.annualized(summa, Rf = 0) # uses geometric returns
