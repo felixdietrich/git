0fef1e232d7a8805c00ec0fadc429a89df769be1
felixdietrich <felix.dietrich@unisg.ch>
2018-01-09 17:51:20 +0100

    1/52 vs. 5/252 is crucial

--- p3/BSfromDelta.R
+++ p3/BSfromDelta.R
@@ -1,3 +1,5 @@
+require(fOptions)
+if(!exists('timedefinition')) timedefinition <- 1/52
 # https://quant.stackexchange.com/questions/29507/calculate-strike-from-black-scholes-delta
 BSStrikeFromDelta <- function(S0, T, r, sigma, delta, right)
 {
@@ -7,15 +9,40 @@ BSStrikeFromDelta <- function(S0, T, r, sigma, delta, right)
   return(strike)
 }
 
-spot <- 1.15
-time <- 1/52
-sigma <- 20/100
+# double check the function
+# spot <- 1.15
+# time <- 1/52
+# sigma <- 20/100
+# GBSGreeks(Selection = "delta", TypeFlag = "c", S = spot, X = BSStrikeFromDelta(spot, time, 0.0, sigma, 0.25, 'C'), Time = time, r = 0, b = 0, sigma = sigma)
+# GBSGreeks(Selection = "delta", TypeFlag = "p", S = spot, X = BSStrikeFromDelta(spot, time, 0.0, sigma, -0.25, 'P'), Time = time, r = 0, b = 0, sigma = sigma)
 
-BSStrikeFromDelta(spot, time, 0.0, sigma, 0.25, 'C')
+put_fromdelta = function(x,y) { BSStrikeFromDelta(as.numeric(x[,'Spot']), timedefinition, 0.0, as.numeric(x[,'Vol'])/100, -(y), 'P') }
+call_fromdelta = function(x,y) { BSStrikeFromDelta(as.numeric(x[,'Spot']), timedefinition, 0.0, as.numeric(x[,'Vol'])/100, y, 'C') }
+# WHAT ABOUT IMPLIED VOLS and BUTTERFLIES HERE
+# asd <- fx[[4]][,c(2,1)]
+# colnames(asd) = c('Vol','Spot')
+# put_fromdelta(asd[1,], 0.25)
+# put_fromdelta(data.frame('Vol'=13.50,'Spot'=1.0296), 0.25)
 
-require(fOptions)
-GBSGreeks(Selection = "delta", TypeFlag = "c", S = spot, X = BSStrikeFromDelta(spot, time, 0.0, sigma, 0.25, 'C'), Time = time, r = 0, b = 0, sigma = sigma)
-GBSGreeks(Selection = "delta", TypeFlag = "p", S = spot, X = BSStrikeFromDelta(spot, time, 0.0, sigma, -0.25, 'P'), Time = time, r = 0, b = 0, sigma = sigma)
+# test if interest rates affect
+# GBSOption(TypeFlag = "p", S = 1.10, X = 1.09, Time = 1/52, r = 0.05, b = 0.03, sigma = 9/100)@price 
 
-getwd()
-source('test.R')
+put_calc = function(x) { 
+  spot <- as.numeric(x[,'Spot']); strike <- as.numeric(x[,'down']); vol <- as.numeric(x[,'Vol'])
+  GBSOption(TypeFlag = "p", S = spot, X = strike, Time = timedefinition, r = 0.00, b = 0.00, sigma = vol/100)@price 
+}
+call_calc = function(x) { 
+  spot <- as.numeric(x[,'Spot']); strike <- as.numeric(x[,'up']); vol <- as.numeric(x[,'Vol'])
+  GBSOption(TypeFlag = "c", S = spot, X = strike, Time = timedefinition, r = 0.00, b = 0.00, sigma = vol/100)@price 
+}
+
+straddle_calc = function(x) { 
+  spot <- as.numeric(x[,'Spot']); strike <- as.numeric(x[,'Spot']); vol <- as.numeric(x[,'Vol'])
+  GBSOption(TypeFlag = "c", S = spot, X = strike, Time = timedefinition, r = 0.00, b = 0.00, sigma = vol/100)@price 
+  # as.numeric(x[,'dayz']/252
+}
+straddle_calc2 = function(x) { 
+  spot <- as.numeric(x[,'Spot']); strike <- as.numeric(x[,'Spot']); vol <- as.numeric(x[,'Vol'])
+  GBSOption(TypeFlag = "c", S = spot, X = strike, Time = 5/252, r = 0.00, b = 0.00, sigma = vol/100)@price # SLIGHTLY HIGHER than 1/52
+}
+
